5d86921154a269aaa010c968452d61922f089089
RandomPigYT <vinecompilation864@gmail.com>
2024-07-16 13:39:03 +0530

    Start defining doom-style renderer's api

--- src/doom-style-renderer/doom-style-renderer.h
+++ src/doom-style-renderer/doom-style-renderer.h
@@ -1,4 +1,41 @@
 #ifndef DOOM_STYLE_RENDERER_H
 #define DOOM_STYLE_RENDERER_H
 
+#include <stdint.h>
+
+struct dsr_Pixel_format {
+  uint8_t bits_per_pixel;
+  uint8_t bytes_per_pixel;
+
+  uint32_t r_mask;
+  uint32_t g_mask;
+  uint32_t b_mask;
+  uint32_t a_mask;
+
+  uint8_t r_bits;
+  uint8_t g_bits;
+  uint8_t b_bits;
+  uint8_t a_bits;
+
+  uint8_t r_shift;
+  uint8_t g_shift;
+  uint8_t b_shift;
+  uint8_t a_shift;
+};
+
+struct dsr_Surface {
+  uint32_t width, height;
+  uint32_t stride; // In bytes
+
+  struct dsr_Pixel_format pixel_format;
+
+  void *pixels;
+};
+
+struct dsr_Scene {
+  /* ... */
+};
+
+void dsr_render(struct dsr_Surface *surface, struct dsr_Scene *scene);
+
 #endif // DOOM_STYLE_RENDERER_H
