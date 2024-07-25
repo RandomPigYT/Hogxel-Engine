3e89ae7dd6f730f612592e0eeb0b11e142d29f0b
RandomPigYT <vinecompilation864@gmail.com>
2024-07-25 20:48:02 +0530

    [WIP]: Implement scene parser

--- src/doom-style-renderer/load-scene.c
+++ src/doom-style-renderer/load-scene.c
@@ -1,5 +1,4 @@
 #include "doom-style-renderer.h"
-
 #define UTIL_FILE_IO_IMPLEMENTATION
 #include "util/fileIO.h"
 
@@ -7,13 +6,14 @@
 #include <stdio.h>
 #include <string.h>
 
-#define ASSERT_VIEW_BOUNDS(str_view, str)                                    \
-  do {                                                                       \
-    assert((((str_view).view >= (str)) || (str_view).view == NULL) &&        \
-           "View out of bounds");                                            \
-                                                                             \
-    assert((str_view).view < (str) + strlen((str)) && "View out of bounds"); \
-                                                                             \
+#define ASSERT_VIEW_BOUNDS(str_view, str_src)                                  \
+  do {                                                                         \
+    assert((((str_view).view >= (str_src).view) || (str_view).view == NULL) && \
+           "View out of bounds");                                              \
+                                                                               \
+    assert((str_view).view < (str_src).view + (str_src).length &&              \
+           "View out of bounds");                                              \
+                                                                               \
   } while (0)
 
 struct StringView {
@@ -29,32 +29,30 @@ enum BlockType {
 
 // str -> Start of the current line
 // len -> size of the entire string containing the line, starting from 'str'
-static uint64_t get_line_len(const char *str, uint64_t len) {
-  const char *temp = str;
-  while (*temp) {
-    if (*temp == '\n') {
-      return temp - str == 0 ? 0 : temp - str;
+static uint64_t get_chunk_len(const char *str, uint64_t len, char chunk_delim) {
+  for (uint64_t i = 0; i < len; i++) {
+    if (str[i] == chunk_delim) {
+      return &(str[i]) - str == 0 ? 0 : &(str[i]) - str;
     }
-
-    temp++;
   }
 
   return len;
 }
 
-static void next_line(struct StringView *view, const char *scene_src) {
-  ASSERT_VIEW_BOUNDS(*view, scene_src);
+static void next_chunk(struct StringView *view,
+                       const struct StringView *scene_src, char chunk_delim) {
+  ASSERT_VIEW_BOUNDS(*view, *scene_src);
 
   if (view->view == NULL) {
     *view = (struct StringView){
-      .view = scene_src,
-      .length = get_line_len(scene_src, strlen(scene_src)),
+      .view = scene_src->view,
+      .length = get_chunk_len(scene_src->view, scene_src->length, chunk_delim),
     };
 
     return;
   }
 
-  const char *scene_src_end = scene_src + strlen(scene_src);
+  const char *scene_src_end = scene_src->view + scene_src->length;
 
   view->view += view->length + 1;
   if (view->view >= scene_src_end) {
@@ -62,7 +60,7 @@ static void next_line(struct StringView *view, const char *scene_src) {
     return;
   }
 
-  view->length = get_line_len(view->view, strlen(view->view));
+  view->length = get_chunk_len(view->view, strlen(view->view), chunk_delim);
 }
 
 static void lstrip_line(struct StringView *view) {
@@ -83,6 +81,14 @@ static void lstrip_line(struct StringView *view) {
   view->length = 0;
 }
 
+static enum BlockType block_type = BLOCK_TYPE_NONE;
+
+static bool parse_line(const struct StringView *line_view) {
+  struct StringView token_view = { 0 };
+
+  return true;
+}
+
 bool dsr_load_scene(const char *scene_path, struct dsr_Scene *scene) {
   (void)scene;
 
@@ -92,23 +98,15 @@ bool dsr_load_scene(const char *scene_path, struct dsr_Scene *scene) {
   }
 
   struct StringView view = { 0 };
-
-  //printf("Lines: \n");
-
-  int line = 0;
   do {
-    next_line(&view, scene_src);
+    next_chunk(&view, &(struct StringView){ scene_src, strlen(scene_src) },
+               '\n');
+
     lstrip_line(&view);
 
     if (view.length) {
-      printf("line %d: ", line);
-      printf("%.*s\n", (int)view.length, view.view);
+      printf("line: %.*s\n", (int)view.length, view.view);
     }
-
-    if (!view.length)
-      line--;
-
-    line++;
   } while (view.view);
 
   free(scene_src);
