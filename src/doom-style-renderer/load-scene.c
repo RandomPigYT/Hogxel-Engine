7ca8a0ada86a6b3775cee8dc70434cf469d4773c
RandomPigYT <vinecompilation864@gmail.com>
2024-07-29 21:25:26 +0530

    [WIP]: Parse tokens

--- src/doom-style-renderer/load-scene.c
+++ src/doom-style-renderer/load-scene.c
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#define ERROR_BUF_SIZE 512
+
 struct StringView {
   const char *view;
   uint64_t length;
@@ -14,8 +16,11 @@ struct StringView {
 
 enum BlockType {
   BLOCK_TYPE_NONE = 0,
+
   BLOCK_TYPE_WALLS,
   BLOCK_TYPE_SCENES,
+
+  BLOCK_COUNT,
 };
 
 struct Tokens {
@@ -24,24 +29,54 @@ struct Tokens {
   uint64_t capacity;
 };
 
+static const char seperators[] = " \t"; // Removed
+static const char special[] = "[]#\n"; // Preseverved as tokens
+
+// clang-format off
+
+#define BLOCKS_SPECS() \
+  X(BEGIN_WALLS)       \
+  X(END_WALLS)         \
+  X(BEGIN_SECTORS)     \
+  X(END_SECTORS)
+
+#define X(name) name,
+
+enum BlockSpec {
+  BLOCKS_SPECS()
+
+  SPEC_COUNT,
+};
+
+#undef X
+
+#define X(name) #name,
+
+static const char *block_specs[] = { 
+	BLOCKS_SPECS()
+};
+
+#undef X
+
+// clang-format on
+
 // Returned dynamic array must be freed by caller
 static struct Tokens tokenize(const char *src) {
-  const char seperators[] = " \t"; // Removed
-  const char special[] = "[]#\n"; // Preseverved as tokens
-
   struct Tokens toks = { 0 };
 
   struct StringView token = { 0 };
   while (*src) {
     if (strstr(seperators, (char[]){ *src, 0 })) {
-      if (token.length)
+      if (token.length) {
         DA_APPEND(&toks, token);
+      }
 
       token = (struct StringView){ 0 };
 
     } else if (strstr(special, (char[]){ *src, 0 })) {
-      if (token.length)
+      if (token.length) {
         DA_APPEND(&toks, token);
+      }
 
       token.view = src;
       token.length = 1;
@@ -50,8 +85,9 @@ static struct Tokens tokenize(const char *src) {
       token = (struct StringView){ 0 };
 
     } else {
-      if (token.view == NULL)
+      if (token.view == NULL) {
         token.view = src;
+      }
 
       token.length++;
     }
@@ -60,6 +96,43 @@ static struct Tokens tokenize(const char *src) {
   }
 
   return toks;
+}
+
+static bool parse(struct Tokens *tokens, struct dsr_Scene *scene, char *error) {
+  DA_TYPE(struct StringView *) line_prev_toks = { 0 };
+
+  bool is_comment = false;
+  for (uint64_t i = 0; i < tokens->count; i++) {
+    assert(DA_AT(*tokens, i).length != 0 &&
+           "This should only contain non-zero length tokens");
+
+    if (DA_AT(*tokens, i).view[0] == '\n') {
+      is_comment = false;
+      DA_FREE(&line_prev_toks);
+
+      continue;
+    }
+
+    if (is_comment) {
+      continue;
+    }
+
+    if (strstr(special, DA_AT(*tokens, i).view)) {
+      switch (DA_AT(*tokens, i).view[0]) {
+        case '[':
+        case ']': {
+          DA_APPEND(&line_prev_toks, &DA_AT(*tokens, i));
+        } break;
+
+        case '#': {
+          is_comment = true;
+        } break;
+
+        default:
+          assert(NULL && "Unreachable");
+      }
+    }
+  }
 }
 
 bool dsr_load_scene(const char *scene_path, struct dsr_Scene *scene) {
